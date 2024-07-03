bbd5e6d284917a3549bb2a0bd984be8f17ba3308
poornima reddy <54319456+Poornimait@users.noreply.github.com>
2024-07-03 18:31:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,23 @@
 const makeChange = (c) => {
+	let change = {
+    q: 0,
+    d: 0,
+    n: 0,
+    p: 0
+  };
+
+  change.q = Math.floor(c / 25);
+  c %= 25;
+
+  change.d = Math.floor(c / 10);
+  c %= 10;
+
+  change.n = Math.floor(c / 5);
+  c %= 5;
+
+  change.p = c;
+
+  return change;
   // your name here
 };
 
